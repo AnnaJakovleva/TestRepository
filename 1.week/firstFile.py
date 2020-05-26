26cc5cc1373708657f15cba2bc260db6e979ba36
AnnaJakovleva <anna.jakovleva7@gmail.com>
2020-05-26 20:43:19 +0300

    First lesson

---
+++ 1.week/firstFile.py
@@ -0,0 +1,74 @@
+#this is a comment
+'''comment
+comment
+comment'''
+'''
+print("hello world!")
+print(5)
+print(5+5)
+print("Testing if this works" + "5")
+print (10*2-15)
+print ("Testing if THIS works" * 10)
+
+MyFirstVariable = 10
+print(MyFirstVariable/2)
+MySecondVariable = "This is my variable"
+print(MySecondVariable)
+
+MySecondVariable=20
+print(MySecondVariable)
+
+firstSide = 10
+print(firstSide*firstSide)
+
+triangleSide = 20
+#a*height/2
+height = 15
+print("The plain of this triangle is :")
+print(triangleSide*height/2)
+
+secondSide = 5
+#a*a*a
+print(secondSide*secondSide*secondSide)
+
+if height<20:
+    print("The height is small")
+if height>500:
+    print("That is very high")
+
+UserInput = int(input("what is your input? "))
+print(UserInput)
+
+if UserInput<20:
+    print("That is a small input")
+
+if UserInput>20:
+    print("That is a big input")
+'''
+'''
+FirstBooleanVariable = True
+if FirstBooleanVariable:
+    print("Our boolean works")
+'''
+'''Write a piece of code, that asks for user input. if that input ,devided by 4 and multiplied by 10,
+ is still smaller than 1000. Then print out the value of the remaining variable number'''
+'''
+UserInputTwo = int(input("Please enter input "))
+print(UserInputTwo)
+
+TheRemaining= UserInputTwo/4*10
+if TheRemaining<1000:
+    print(TheRemaining)
+'''
+'''Find out if this number is divisible by 4 but not by 100 and get a full value'''
+
+UserSecondInput = int(input("Enter your number "))
+if UserSecondInput % 4 == 0:
+    print ("Is divisible by 4")
+    if UserSecondInput % 100 != 0:
+        print ("This is the value")
+
+
+
+
+
